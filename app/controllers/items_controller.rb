fdd0af70677bf15695cf65793ea6e8aa84e84776
bu23 <70064197+bu23@users.noreply.github.com>
2020-11-10 23:42:47 +0900

    active_storage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,20 @@
 class ItemsController < ApplicationController
   def index
-    @item = Item.all
+    @item = Item.new
   end
 
   def new
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(params[item_params])
+    @item.save
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(::name, :text, :category_id, :item_status_id, :burder_amount_id, :shipment_source_id, :delivery_schedule_id).merge(user_id: current_user.id)
+  end
 end
