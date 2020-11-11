40a7862e6b7f61e9db132bbb44e3c5b05de147b9
bu23 <70064197+bu23@users.noreply.github.com>
2020-11-11 22:35:39 +0900

    item_statusの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
+  belongs_to :category
+  belongs_to :item_status
 
   belongs_to :user
   has_one_attached :image
@@ -12,7 +13,7 @@ class Item < ApplicationRecord
   
   with_options numericality: { other_than: 1 } do
    validates :category_id
-  #validates :item_status_id
+   validates :item_status_id
   #validates :burder_amount_id
   #validates :shipment_source_id
   #validates :delivery_schedule_id
