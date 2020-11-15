b11ab63d98b8f8afb7d85b764eb7791ce14c2077
bu23 <70064197+bu23@users.noreply.github.com>
2020-11-15 15:55:14 +0900

    修正

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,8 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.name }
     email                 { Faker::Internet.free_email }
-    password = Faker::Lorem.characters(number: 10, min_alpha: 4, min_numeric: 1)
-    password              { password }
+    password              { Faker::Lorem.characters(number: 10, min_alpha: 4, min_numeric: 1) }
     password_confirmation { password }
     family_name           { '山田' }
     first_name            { '太郎' }
