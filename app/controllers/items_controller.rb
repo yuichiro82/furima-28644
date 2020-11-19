0a1415357194ebea96cdbe06db819a0e4d6d2bfb
bu23 <70064197+bu23@users.noreply.github.com>
2020-11-19 22:52:06 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :show, :edit]
 
   def index
-    @item = Item.all.order("created_at DESC")
+    @item = Item.all.order('created_at DESC')
   end
 
   def new
