3fdc3e355f8e72de37aa56bed526510a52805d7a
bu23 <70064197+bu23@users.noreply.github.com>
2020-11-21 22:25:50 +0900

    addressテーブル作成

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -14,7 +14,7 @@ class OrderAddress
   validates :building_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
 
   def save
-   Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_nubmer: phone_nubmer, order_id: order.id)
    Order.create(user_id: user.id, item_id: item.id, address_id: address.id)
+   Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_nubmer: phone_nubmer, order_id: order.id)
   end
 end
--- db/migrate/20201121082100_create_orders.rb
+++ db/migrate/20201121082100_create_orders.rb
@@ -1,9 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.integer :user_id
-      t.integer :item_id
-      t.integer :address_id
+      t.references :user,      null: false, foreign_key: true
+      t.references :item,      null: false, foreign_key: true
       t.timestamps
     end
   end
