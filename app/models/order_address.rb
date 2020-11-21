8e414b99db372a1cb6afde01f827062662a16f12
bu23 <70064197+bu23@users.noreply.github.com>
2020-11-21 17:33:38 +0900

    formオブジェクト・orderテーブル実装

---
+++ app/models/order_address.rb
@@ -0,0 +1,20 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code,:prefecture_id, :city, :house_number, :building_name, :phone_nubmer
+
+  with_options presence: true do
+    # 「address」に関するバリデーション
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :house_number, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :phone_nubmer, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}
+  end
+
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :building_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+
+  def save
+   Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_nubmer: phone_nubmer, order_id: order.id)
+   Order.create(user_id: user.id, item_id: item.id, address_id: address.id)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
+  has_many :orders
 
   validates_format_of :password, with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/
   with_options presence: true do
