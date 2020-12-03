42c625298ae6d1969a2298c439de86addc983226
bu23 <70064197+bu23@users.noreply.github.com>
2020-12-03 21:13:07 +0900

    修正２

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -8,6 +8,7 @@ class OrderAddress
     validates :house_number
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
     validates :user_id
+    validates :item_id
     validates :token
   end
   validates :prefecture_id,  numericality: { other_than: 0 }
